93ac12ef355d5d587daf911ceb0dd3cc07c2d73c
xiaolong.wang <xiaolong.wang@fenrir-inc.com.cn>
2024-10-17 10:15:18 +0800

    apply astro theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,51 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-
-import sitemap from '@astrojs/sitemap';
-
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import tailwind from '@astrojs/tailwind'
+import sitemap from '@astrojs/sitemap'
+import { remarkReadingTime } from './src/utils/remarkReadingTime.ts'
+import remarkUnwrapImages from 'remark-unwrap-images'
+import rehypeExternalLinks from 'rehype-external-links'
+import expressiveCode from 'astro-expressive-code'
+import { expressiveCodeOptions } from './src/site.config'
+import icon from 'astro-icon'
 import cloudflare from '@astrojs/cloudflare';
+
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap()],
+  integrations: [
+    expressiveCode(expressiveCodeOptions),
+    tailwind({
+			applyBaseStyles: false
+		}),
+		sitemap(),
+		mdx(),
+		icon()
+  ],
   output: 'server',
   adapter: cloudflare(),
+  markdown: {
+		remarkPlugins: [remarkUnwrapImages, remarkReadingTime],
+		rehypePlugins: [
+			[
+				rehypeExternalLinks,
+				{
+					target: '_blank',
+					rel: ['nofollow, noopener, noreferrer']
+				}
+			]
+		],
+		remarkRehype: {
+			footnoteLabelProperties: {
+				className: ['']
+			}
+		}
+	},
+	prefetch: true,
+	output: 'server',
+	adapter: vercel({
+		webAnalytics: { enabled: true }
+	})
 });
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,13 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
+export const SITE_TITLE = '小伞的博客'
+export const SITE_DESCRIPTION = '欢迎来到我的博客，我是小伞'
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const NAME = '小伞'
+
+export const HOME = '主页'
+export const POST = '文章'
+export const DOC = '文档'
+export const ABOUT = '关于'
+export const TAG = '标签'
+
+export const EXPERIENCE = '经历'
+export const SKILL = '技能'
